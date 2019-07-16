d74c9b804661c33d8599dcd1e22d2eb4f0551dff
Антон Иконников <ika@webcrt.ru>
2019-07-16 18:06:28 +0500

    update class

--- CMpr.php
+++ CMpr.php
@@ -2,109 +2,113 @@
 
 class CMpr
 {
-    private static function __mprClearKey($arData) {
+    static $margin = 20;
+
+    private static function __clearKey($arData) {
         $arResult = $arData;
      
         if (is_array($arData)) {
             $arResult = array();
             foreach ($arData as $key => $val) {
                 if (is_integer($key) || is_string($key) && $key[0] != '~') {
-                    $arResult[$key] = self::__mprClearKey($val);
+                    $arResult[$key] = self::__clearKey($val);
                 }
             }
         }
         return $arResult;
     }
-    
-    private static function __mprPrint($arData) {
-        if (is_object($arData) || is_array($arData)) {
-            echo '<div>';
-                echo '<details open>';
-                    echo '<summary style="outline:none!important;cursor:pointer">';
-                        echo (is_object($arData)) ? '<span style="color:#c678dd;">' . get_class($arData) . ' Object {' . count((array)$arData) . '}</span>' : '<span style="color:#e06c75">Array [' . count($arData) . ']</span>';
-                    echo '</summary>';
-                    self::__mprPrintRec($arData);
-                echo '</details>';
+
+    private static function __printSimpleLine($value, $key, $isObject) {
+        $sType = gettype($value);
+        if($value === "NO DATA!!!") {
+            $sType = "ERROR";
+        }
+        $sChars = "";
+        $sColor = "";
+        switch($sType) {
+            case 'string':
+                $sColor = '#61afef';
+                $value = str_replace(chr(13), '', $value);
+                $value = str_replace(chr(10), '', $value);
+                $sChars = ' <small>' . iconv_strlen($value) . '</small>';
+            break;
+            case 'integer':
+                $sColor = '#98c379';
+            break;
+            case 'double':
+                $sColor = '#98c379';
+            break;
+            case 'boolean':
+                $sColor = '#d19a66';
+                $value = $value ? 'TRUE' : 'FALSE';
+            break;
+            case 'NULL':
+                $sColor = '#d19a66';
+                $value = 'NULL';
+            break;
+            case 'ERROR':
+                $sColor = '#e06c75';
+            break;
+        }
+
+        if ($key) {
+            if ($isObject) {
+                echo '<div style="margin-left:' . self::$margin . 'px"><span>' . $key . '</span> : ';
+            } else {
+                echo '<div style="margin-left:' . self::$margin . 'px"><span>[' . $key . ']</span> => ';
+            }
+        }
+        echo '<span style="display:inline-table;color:' . $sColor . '">' . $value . '</span> <span style="opacity:0.5">(' . $sType . $sChars . ')</span>';
+        if ($key) {
             echo '</div>';
-        } else {
-            $sType = gettype($arData);
-            if($arData === "NO DATA!!!") {
-                $sType = "ERROR";
+        }
+
+    }
+
+    private static function __printArrayLine($value, $key, $isObject) {
+        $margin = '';
+        if ($key) {
+            if ($isObject) {
+                $key = $key . ' : ';
+            } else {
+                $key = '[' . $key . '] => ';
             }
-            $sChars = "";
-            $sColor = "";
-            switch($sType) {
-                case 'string':
-                    $sColor = '#61afef';
-                    $arData = str_replace(chr(13), '', $arData);
-                    $arData = str_replace(chr(10), '', $arData);
-                    $sChars = ' <small>' . iconv_strlen($arData) . '</small>';
-                break;
-                case 'integer':
-                    $sColor = '#98c379';
-                break;
-                case 'double':
-                    $sColor = '#98c379';
-                break;
-                case 'boolean':
-                    $sColor = '#d19a66';
-                    $arData = $arData ? 'TRUE' : 'FALSE';
-                break;
-                case 'NULL':
-                    $sColor = '#d19a66';
-                    $arData = 'NULL';
-                break;
-                case 'ERROR':
-                    $sColor = '#e06c75';
-                break;
-            }
-            echo '<span style="color:' . $sColor . '">' . $arData . '</span> <span style="opacity:0.5">(' . $sType . $sChars . ')</span>';
+            $margin = ' style="margin-left:' . self::$margin . 'px"';
+            echo '<div>';
+        }
+        echo '<details open' . $margin . '>';
+            echo '<summary style="outline:none!important;cursor:pointer">';
+                echo (is_object($value)) ? $key . '<span style="color:#c678dd;">' . get_class($value) . ' Object {' . count((array)$value) . '}</span>' : $key . '<span style="color:#e06c75">Array [' . count($value) . ']</span>';
+            echo '</summary>';
+            self::__printRec($value);
+        echo '</details>';
+        if ($key) {
+            echo "</div>";
         }
     }
     
-    private static function __mprPrintRec($arData, $margin = 20){
+    private static function __print($arData, $key = '', $isObject = false) {
+        if (is_object($arData) || is_array($arData)) {
+            self::__printArrayLine($arData, $key, $isObject);
+        } else {
+            self::__printSimpleLine($arData, $key, $isObject);
+        }
+    }
+    
+    private static function __printRec($arData) {
         if (!is_object($arData) && !is_array($arData)) {
             return;
         }
-        $arData = self::__mprClearKey($arData);
-     
+        $arData = self::__clearKey($arData);
+
+        $isObject = false;
+        if (is_object($arData)) {
+            $isObject = true;
+        }
+
         echo '(';
         foreach ($arData as $key => $value) {
-            if (is_object($value) || is_array($value)) {
-                echo '<details open style="margin-left:' . $margin . 'px">';
-                    echo '<summary style="outline:none!important;cursor:pointer">';
-                        echo (is_object($value)) ? '[' . $key . '] => <span style="color: #c678dd;">' . get_class($value) . ' Object {' . count((array)$value) . '}' : '[' . $key . ']</span> => <span style="color: #e06c75">Array [' . count($value) . ']</span>';
-                    echo '</summary>';
-                    self::__mprPrintRec($value, $margin + 10);
-                echo '</details>';
-            } else {
-                $sType = gettype($value);
-                $sChars = "";
-                $sColor = "";
-                switch ($sType) {
-                    case 'string':
-                        $sColor = '#61afef';
-                        $value = str_replace(chr(13), '', $value);
-                        $value = str_replace(chr(10), '', $value);
-                        $sChars = ' <small>' . iconv_strlen($value) . '</small>';
-                    break;
-                    case 'integer':
-                        $sColor = '#98c379';
-                    break;
-                    case 'double':
-                        $sColor = '#98c379';
-                    break;
-                    case 'boolean':
-                        $sColor = '#d19a66';
-                        $value = $value ? 'TRUE' : 'FALSE';
-                    break;
-                    case 'NULL':
-                        $sColor = '#d19a66';
-                        $value = 'NULL';
-                    break;
-                }
-                echo '<div style="margin-left:' . $margin . 'px"><span>[' . $key . ']</span> => <span style="display:inline-table;color:' . $sColor . '">' . $value . '</span> <span style="opacity: 0.5">(' . $sType . $sChars . ')</span></div>';
-            }
+            self::__print($value, $key, $isObject);
         }
         if (count($arData) == 0) {
             echo '<br>';
@@ -123,9 +127,9 @@ class CMpr
         if ($nNumargs < 1) {
             $arData = "NO DATA!!!";
         } elseif ($nNumargs == 1) {
-            $arData = self::__mprClearKey($arArgs[0]);
+            $arData = self::__clearKey($arArgs[0]);
         } else {
-            $arData = self::__mprClearKey($arArgs[0]);
+            $arData = self::__clearKey($arArgs[0]);
             unset($arArgs[0]);
      
             $nDie = array_search("die", $arArgs, true);
@@ -167,7 +171,7 @@ class CMpr
         echo "<span style='padding:5px 10px 10px;float:left;opacity:0.5;font-family:monospace;word-wrap:break-word;max-width:100%;'>" . str_replace($_SERVER["DOCUMENT_ROOT"], '', $arDebug['file']) . " [" . $arDebug['line'] . "]</span>";
      
         echo "<pre style='background:#282c34;color:#abb2bf;border:0;border-radius:0;margin:29px 0 0;font-family:monospace;font-size:13px;font-weight:400;max-height:500px;overflow:auto;clear:both;padding:5px;'>";
-            self::__mprPrint($arData);
+            self::__print($arData);
         echo "</pre>";
      
         echo "</div>";
