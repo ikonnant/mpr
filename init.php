7dc754a0f799eb9ecbab1711df5557f8b96c2bac
ikonnant <52152270+ikonnant@users.noreply.github.com>
2019-07-20 18:29:20 +0500

    Update init.php
    Refactoring update

--- init.php
+++ init.php
@@ -6,13 +6,14 @@ spl_autoload_register(function ($className) {
 
 //class autoload Bitrix
 CModule::AddAutoloadClasses(
-	'',
-	array(
-        'CMpr' => '/local/php_interface/CMpr.php'
-    )
+    '',
+    ['CMpr' => '/local/php_interface/CMpr.php']
 );
 
 //function short class call
 function mpr() {
-    call_user_func_array('CMpr::mpr', func_get_args());
+    call_user_func_array(
+	'CMpr::mpr',
+	func_get_args()
+    );
 }
